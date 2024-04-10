921dad5812125b88d9474cb5f0327f7175e6fdeb
Ester Cristina da Cunha e. Silva <ester.cristina.silva@accenture.com>
2024-04-10 09:02:36 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,74 @@
+import { Alert, Image, StatusBar, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Link, Redirect } from "expo-router";
+
+import { api } from "@/server/api";
+import { useBadgeStore } from "@/store/badge-store";
+
+import { colors } from "@/styles/colors";
+
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { useState } from "react";
+
+export default function Home() {
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const badgeStore = useBadgeStore();
+
+  async function handleAcessCredential() {
+    try {
+      if (!code.trim()) {
+        return Alert.alert("Ingresso", "Informe o código do ingresso!");
+      }
+      setLoading(true);
+
+      const { data } = await api.get(`/attendees/${code}/badge`);
+      badgeStore.save(data.badge);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      Alert.alert("Ingresso", "Ingresso não encontrado!");
+    }
+  }
+
+  if (badgeStore.data?.checkInURL) {
+    return <Redirect href="/ticket" />;
+  }
+
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content" />
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field
+            placeholder="Código do Ingresso"
+            onChangeText={setCode}
+          />
+        </Input>
+        <Button
+          title="Acessar Credencial"
+          onPress={handleAcessCredential}
+          isLoading={loading}
+        />
+        <Link
+          href="/register"
+          className="text-gray-100 font-base font-bold text-center mt-8"
+        >
+          Ainda não possui ingresso?
+        </Link>
+      </View>
+    </View>
+  );
+}
---
+++ src/components/credential.tsx
@@ -0,0 +1,83 @@
+import {
+  Image,
+  ImageBackground,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+import { colors } from "@/styles/colors";
+
+import { BadgeStore } from "@/store/badge-store";
+
+import { QRCode } from "@/components/qrcode";
+
+type Props = {
+  data: BadgeStore;
+  image?: string;
+  onChangeAvatar?: () => void;
+  onShowQRCode?: () => void;
+};
+
+export function Credential({ data, onChangeAvatar, onShowQRCode }: Props) {
+  return (
+    <View className="w-full self-stretch items-center">
+      <Image
+        source={require("@/assets/ticket/band.png")}
+        className="w-24 h-52 z-10"
+      />
+
+      <View className="bg-black/20 self-stretch items-center pb-6 border border-white/10 mx-3 rounded-2xl -mt-5">
+        <ImageBackground
+          source={require("@/assets/ticket/header.png")}
+          className="px-6 py-8 h-40 items-center self-stretch border-b border-white/10 overflow-hidden"
+        >
+          <View className="w-full flex-row items-center justify-between">
+            <Text className="text-zinc-50 text-sm font-bold">
+              {data.eventTitle}
+            </Text>
+            <Text className="text-zinc-50 text-sm font-bold">#{data.id}</Text>
+          </View>
+          <View className="w-40 h-40 bg-black rounded-full" />
+        </ImageBackground>
+        {data.image ? (
+          <TouchableOpacity activeOpacity={0.9} onPress={onChangeAvatar}>
+            <Image
+              source={{ uri: data.image }}
+              className="w-36 h-36 rounded-full -mt-24"
+            />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            activeOpacity={0.9}
+            className="w-36 h-36 rounded-full -mt-24 bg-gray-400 items-center justify-center"
+            onPress={onChangeAvatar}
+          >
+            <Feather name="camera" color={colors.green[400]} size={32} />
+          </TouchableOpacity>
+        )}
+
+        <Text className="font-bold text-2xl text-zinc-50 mt-4">
+          {data.name}
+        </Text>
+        <Text className="font-regular text-base text-zinc-300 mb-4">
+          {data.email}
+        </Text>
+
+        <QRCode value={data.checkInURL} size={120} />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="mt-6"
+          onPress={onShowQRCode}
+        >
+          <Text className="font-body text-orange-500 text-sm">
+            {" "}
+            Ampliar QRCode
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
